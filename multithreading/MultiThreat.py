5a89b6e104a5103c419c2f20ab34809fbd161619
shivDj <sheevendra.singhraul@gmail.com>
2021-03-28 10:42:54 +0530

    multithreading code refectored

--- multithreading/MultiThreat.py
+++ multithreading/MultiThreat.py
@@ -7,13 +7,14 @@ purpose:-multithreading implementation
 @since:-28-03-2021
 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 """
+# Thread and time modules are imported
 from threading import *
 from time import  sleep
-class hello(Thread):
-    def run(self):
-        for i in range (5):
+class hello(Thread):  #class created with name hello
+    def run(self):    # method run is created
+        for i in range (5): #for loop will run number of argument is passed to it
             print("Hello")
-            sleep(1)
+            sleep(1)   #sleep method ill hold the execution for 1 second
 
 class hi(Thread):
     def run(self):
@@ -21,12 +22,14 @@ class hi(Thread):
             print("hi")
             sleep(1)
 
-h=hello()
-hi=hi()
 
-h.start()
-sleep(0.3)
-hi.start()
+if __name__=="__main__":
+    h=hello()
+    hi=hi()
 
-h.join()
-hi.join()
+    h.start()
+    sleep(0.3)
+    hi.start()
+
+    h.join()
+    hi.join()
