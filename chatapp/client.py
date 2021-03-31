63506d910d9ddb41afe7c61a726e980adfef5c1c
shivDj <sheevendra.singhraul@gmail.com>
2021-03-31 10:42:38 +0530

    refactored chatapp data with database connectivity

--- chatapp/client.py
+++ chatapp/client.py
@@ -1,14 +1,36 @@
 import threading
 import socket
+import mysql.connector
+
 alias = input('Choose an alias >>> ')
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(('127.0.0.1', 59000))
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="shiv@123",
+    database="chat",
+    auth_plugin="mysql_native_password")
+
+try:
+    print("connection is successful ",mydb.is_connected())
+
+except TypeError as e:
+    print("connection is not successful",e)
+
+mycursor = mydb.cursor()
 
 
 def client_receive():
     while True:
         try:
             message = client.recv(1024).decode('utf-8')
+            string_message=str(message)
+            query = input("insert into talking (chat)values(?)",(string_message,))
+            mycursor.execute(query)
+            mydb.commit()
+
             if message == "alias?":
                 client.send(alias.encode('utf-8'))
             else:
@@ -22,6 +44,11 @@ def client_receive():
 def client_send():
     while True:
         message = f'{alias}: {input("")}'
+        string_message = str(message)
+        query = input("insert into talking (chat) values (?)", (string_message,))
+        mycursor.execute(query)
+        mydb.commit()
+
         client.send(message.encode('utf-8'))
 
 
